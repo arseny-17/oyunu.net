d40f4581259798de0033b5b6e63454a37238f687
Arsus <arseny-17@yandex.ru>
2023-06-06 12:46:28 +0300

    banner media

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -10,45 +10,42 @@ export default function Banner(){
     const {amp, mainLink} = context
 
     return (
-        <>
 
-           
-  
-        <div className="banner">
-            <div className="info">
-                <p className="mainText">Risksiz <span>bahis</span></p>
-                <p className="shadow">Ücretler</p>
-                <span className="secondText">Her durumda zafer senin</span>
-                <div className="button">
-                    <Button 
-                        text="Giriş"
-                        amp={amp} 
-                        link={mainLink}
-                        areaLabel="Giriş"
-                        split="buttonBanner"
-                        buttonStyle="btn-banner- banner-btn"
-                    />
-                </div>
-                <div className="list">
-                    <div className="listItem">
-                        <span className="number">1</span>
-                        <span className="name">GİR</span>
-                    </div>
-                    <div className="listItem">
-                        <span className="number">2</span>
-                        <span className="name">BAHİS YAP</span>
-                    </div>
-                    <div className="listItem">
-                        <span className="number">3</span>
-                        <span className="name">PARAYI AL</span>
-                    </div>
-                </div>
+    <div className="banner">
+      <div className="banner__wrapper">
+        <div className="info">
+          <div className="info__text">
+            <p className="mainText">Risksiz <span>bahis</span></p>
+            <p className="shadow">Ücretler</p>
+            <span className="secondText">Her durumda zafer senin</span>
+            <div className="button">
+                <Button 
+                    text="Giriş"
+                    amp={amp} 
+                    link={mainLink}
+                    areaLabel="Giriş"
+                    split="buttonBanner"
+                    buttonStyle="btn-banner- banner-btn"
+                />
             </div>
-            <div className="bannerImg">
-                <ImageWrap imgsrc="/uploads/img/girl.webp" imgwidth={600} imgheight={300} imgalt="banner" />
+          </div>
+          <div className="list">
+            <div className="listItem">
+              <span className="number">1</span><span className="name">GİR</span>
             </div>
+            <div className="listItem">
+              <span className="number">2</span><span className="name">BAHİS YAP</span>
+            </div>
+            <div className="listItem">
+              <span className="number">3</span><span className="name">PARAYI AL</span>
+            </div>
+          </div>
         </div>
-        </>
+        <div className="bannerImg">
+            <ImageWrap imgsrc="/uploads/img/girl.webp" imgwidth={600} imgheight={300} imgalt="banner" />
+        </div>
+      </div>
+    </div>
     )
 
 }
